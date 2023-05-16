e75639e6520917c477c0e184b63af72916c1dbc9
Thedath Oudarya <thedath.oudarya@gmail.com>
2023-05-16 11:55:20 +0530

    Setup example API
    Signed-off-by: Thedath Oudarya <thedath.oudarya@gmail.com>

--- lib/tenant-isolation-walkthrough-stack.ts
+++ lib/tenant-isolation-walkthrough-stack.ts
@@ -1,16 +1,54 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {
+  aws_lambda as lambda,
+  aws_apigateway as apiGateway,
+  aws_dynamodb as dynamodb,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import { exampleLambdaDir } from "./exampleLambda";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class TenantIsolationWalkthroughStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class TenantIsolationWalkthroughStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ddb = new dynamodb.Table(this, "ExampleTable", {
+      tableName: "example",
+      partitionKey: {
+        name: "partKey1",
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: "sortKey1",
+        type: AttributeType.STRING,
+      },
+    });
+    ddb.addGlobalSecondaryIndex({
+      indexName: "gsi1",
+      partitionKey: {
+        name: "partKey2",
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: "sortKey2",
+        type: AttributeType.STRING,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TenantIsolationWalkthroughQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const exampleLambda = new lambda.Function(this, "ExampleLambda", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset(exampleLambdaDir),
+      handler: "index.handler",
+      environment: {
+        DDB_TABLE_NAME: ddb.tableName,
+      },
+    });
+    ddb.grantFullAccess(exampleLambda);
+
+    const api = new apiGateway.RestApi(this, "ExampleAPI");
+
+    api.root.addMethod("POST", new apiGateway.LambdaIntegration(exampleLambda));
   }
 }
